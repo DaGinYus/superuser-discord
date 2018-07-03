2ace0f5729ce890a0b3efff2ed9e9d82ad754d1b
Matthew Wong <matthew@adveca.com>
2018-07-03 11:23:00 -0700

    fixed some bugs

--- su-discord.py
+++ su-discord.py
@@ -14,18 +14,17 @@ async def on_ready():
 @commands.has_any_role("sudoers")
 async def sudo(ctx, *args):
     if args[0] == 'userdel':
-        if len(args) >= 2:
+        if len(args) != 1:
             try:
-                uname = args[1:len(args)-1]
+                uname = args[1:len(args)]
                 usr = await commands.MemberConverter().convert(ctx, " ".join(uname))
+                print("Kicking " + str(usr))
                 await discord.Member.kick(usr)
                 await ctx.send("{} has been kicked!".format(usr))
-            except commands.errors.BadArgument as e:
-                await ctx.send("userdel: {}".format(e.lower()))
             except Exception as e:
                 await ctx.send("error: {}".format(e))
         else:
-            await ctx.send("userdel: username to kick required")
+            await ctx.send("userdel: incorrect number of arguments ({})".format(len(args)))
     else:
         await ctx.send("{}: command not found".format(args[0]))
 
